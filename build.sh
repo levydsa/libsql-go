a184ea0b70760cee52ef5462b5f545097dd16718
Levy A. <levyddsa@gmail.com>
2024-10-29 15:15:00 -0300

    fix: upadate libs

--- build.sh
+++ build.sh
@@ -6,19 +6,19 @@ cd libsql-c
 
 cargo zigbuild --target aarch64-apple-darwin --release
 cargo zigbuild --target x86_64-apple-darwin --release
-RUSTFLAGS="-C target-feature=-crt-static" cargo zigbuild --target aarch64-unknown-linux-musl --release
-RUSTFLAGS="-C target-feature=-crt-static" cargo zigbuild --target x86_64-unknown-linux-musl --release
+cargo zigbuild --target aarch64-unknown-linux-gnu --release
+cargo zigbuild --target x86_64-unknown-linux-gnu --release
 
 rm -rf ../lib
 
 mkdir -p \
-  ../lib/aarch64-unknown-linux-musl \
-  ../lib/x86_64-unknown-linux-musl \
+  ../lib/aarch64-unknown-linux-gnu \
+  ../lib/x86_64-unknown-linux-gnu \
   ../lib/aarch64-apple-darwin \
   ../lib/x86_64-apple-darwin \
 
 cp ./libsql.h ../lib/libsql.h
-cp ./target/x86_64-unknown-linux-musl/release/liblibsql.a ../lib/x86_64-unknown-linux-musl/
-cp ./target/aarch64-unknown-linux-musl/release/liblibsql.a ../lib/aarch64-unknown-linux-musl/
+cp ./target/x86_64-unknown-linux-gnu/release/liblibsql.a ../lib/x86_64-unknown-linux-gnu/
+cp ./target/aarch64-unknown-linux-gnu/release/liblibsql.a ../lib/aarch64-unknown-linux-gnu/
 cp ./target/x86_64-apple-darwin/release/liblibsql.a ../lib/x86_64-apple-darwin/
 cp ./target/aarch64-apple-darwin/release/liblibsql.a ../lib/aarch64-apple-darwin/
--- libsql.go
+++ libsql.go
@@ -7,8 +7,8 @@ package libsql
 #cgo CFLAGS: -I${SRCDIR}/lib/
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/lib/x86_64-apple-darwin
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/lib/aarch64-apple-darwin
-#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/x86_64-unknown-linux-musl
-#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/aarch64-unknown-linux-musl
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/x86_64-unknown-linux-gnu
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/aarch64-unknown-linux-gnu
 #cgo LDFLAGS: -llibsql
 #cgo darwin LDFLAGS: -framework Security
 #cgo darwin LDFLAGS: -framework CoreFoundation
